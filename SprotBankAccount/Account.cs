477b4cdac7dea16e2531f1d5bd1be19b140e2b84
Serj Prots <serganas995@gmail.com>
2022-04-28 17:11:18 +0300

    Добавьте файлы проекта.

---
+++ SprotBankAccount/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprotBankAccount
+{
+    public delegate void AccountHandler(string message);
+
+    public class Account
+    {
+        public int Sum { get; set; }
+
+        AccountHandler taken;
+
+        public Account (int sum)=> Sum = sum;
+
+        public void RegisterHandler(AccountHandler del)
+        {
+            taken = del;
+        }
+
+        public void Add(int sum) => Sum += sum;
+
+        public void Take(int sum)
+        {
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                taken?.Invoke($"Со счета списано {sum} у.е.");
+
+            }
+            else
+            {
+                taken?.Invoke($"Недостаточно средств. Баланс {Sum} у.е.");
+            }
+        }
+
+        public void Print(string message) => Console.WriteLine(message);
+    }
+}
---
+++ SprotBankAccount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SprotBankAccount
+{
+
+    
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(100);
+            account.RegisterHandler(account.Print);
+            account.Add(50);
+            account.Take(200);
+            account.Take(150);
+            Console.ReadLine();
+        }
+       
+    }
+}
